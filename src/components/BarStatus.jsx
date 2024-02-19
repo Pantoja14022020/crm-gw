45e2c0bf5e405e0e241bc4f6010986af1b1a9a67
Pantoja14022020 <tl419411@uaeh.edu.mx>
2024-02-19 16:11:05 -0500

    me quede en el bar status, coloque las flechitas, lo estaba maquetando

--- src/components/BarStatus.jsx
+++ src/components/BarStatus.jsx
@@ -1,3 +1,5 @@
+import { GrFormNext } from "react-icons/gr";
+
 function BarStatus({stages}){
     return(
         <nav className="barStatus">
@@ -7,6 +9,7 @@ function BarStatus({stages}){
                         <li key={id} className={paint ? 'painted' : ''}>
                             <h2>{title}</h2>
                             <p>{description}</p>
+                            <div className="next-icon"><GrFormNext /></div>
                         </li>
                     ))
                 }
